3cb2f758f186cb1feb7cf29563aeecf34421b37f
vivapyk <git hub vivapyk@gmail.com>
2019-01-21 18:01:28 +0900

    7강 끝

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ def lnglat_validator(value):
 
 # Create your models here.
 class Post(models.Model):
+    author = models.CharField(max_length=20)
     title = models.CharField(max_length=100, verbose_name= '제목', # 길이제한 100
   help_text='포스팅 제목을 입력하세요. 100자 내외')
     content = models.TextField(verbose_name='내용') #길이제한 없음
