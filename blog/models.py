9f247afc1a2b9f32d921f0cd9e5df22a5b84adb0
vivapyk <git hub vivapyk@gmail.com>
2019-01-21 16:27:30 +0900

    blog.models Post모델 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name= '제목', # 길이제한 100
+  help_text='포스팅 제목을 입력하세요. 100자 내외')
+    content = models.TextField(verbose_name='내용') #길이제한 없음
+    created_at = models.DateTimeField(auto_now_add=True) #최초 생성시간 저장
+    updated_at = models.DateTimeField(auto_now = True)  #갱신 때마다 시간 저장
+
+
+
--- blog/url.py
+++ blog/url.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import re_path
 from .import views
 
 urlpatterns = [
-    url(r'^$', views.post_list),
+    re_path(r'^$', views.post_list),
 ]
