d6268c9f8e72dde9ba3bf63ee38278001f759b4c
vivapyk <git hub vivapyk@gmail.com>
2019-01-23 02:15:01 +0900

    get_object_or_404

--- blog/url.py
+++ blog/url.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     re_path(r'^$', views.post_list),
+    re_path(r'^(?P<id>\d+)/$', views.post_detail),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Post
+from django.http import Http404
 # Create your views here.
 def post_list(request):
     qs = Post.objects.all()
@@ -11,4 +12,15 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {
         'post_list': qs,
         'q': q,
+    })
+
+def post_detail(request, id):
+    # try:
+    #     post = Post.objects.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404
+
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'blog/post_detail.html', {
+        'post': post,
     })
