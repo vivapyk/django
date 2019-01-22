a8c4f647d4d0c1478587ee8a76c6a25b0d3811eb
vivapyk <git hub vivapyk@gmail.com>
2019-01-22 22:06:17 +0900

    admin mark_safe

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post
+from django.utils.safestring import mark_safe
 # Register your models here.
 
 admin.site.register(Post)
@@ -8,5 +9,9 @@ admin.site.unregister(Post)
 @admin.register(Post)
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'created_at', 'updated_at']
+    list_display = ['id', 'title','content_size', 'created_at', 'updated_at']
 # admin.site.register(Post, PostAdmin)
+    def content_size(self, post):
+        return mark_safe('<strong>{}</strong>글자'.format(len(post.content)))
+    content_size.short_description = '글자수'
+    #content_size.allow_tags = True
