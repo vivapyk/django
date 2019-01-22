2b5682b1ee254fa5ccc55ac01bbf8fb4fc3034ac
vivapyk <git hub vivapyk@gmail.com>
2019-01-22 21:36:50 +0900

    admin register

--- blog/admin.py
+++ blog/admin.py
@@ -3,3 +3,10 @@ from .models import Post
 # Register your models here.
 
 admin.site.register(Post)
+admin.site.unregister(Post)
+
+@admin.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at', 'updated_at']
+# admin.site.register(Post, PostAdmin)
