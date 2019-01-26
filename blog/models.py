956927efbb1916fc858382375158e238d2738137
vivapyk <git hub vivapyk@gmail.com>
2019-01-27 03:49:14 +0900

    blog post모델에 user추가

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 import re
 from django.forms import ValidationError
@@ -16,7 +17,7 @@ class Post(models.Model):
         ('w', 'Withdrawn'),
     )
 
-    author = models.CharField(max_length=20)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, verbose_name='제목',# 길이제한 100
   help_text='포스팅 제목을 입력하세요. 100자 내외')
     content = models.TextField(verbose_name='내용') #길이제한 없음
