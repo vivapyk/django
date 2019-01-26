d3b2ed5ffc76176bd57d1c54893efcab57f9ccda
vivapyk <git hub vivapyk@gmail.com>
2019-01-27 03:25:16 +0900

    account 앱에 모델 생성, 등록

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from accounts.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+
+
+
+
