b82d623841b6860f16f4aacfccdd4ea7b8489f02
vivapyk <git hub vivapyk@gmail.com>
2019-01-28 03:57:43 +0900

    get_absolute_url

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 import re
 from django.forms import ValidationError
+from django.urls import reverse
 
 
 def lnglat_validator(value):
@@ -19,7 +20,7 @@ class Post(models.Model):
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, verbose_name='제목',# 길이제한 100
-  help_text='포스팅 제목을 입력하세요. 100자 내외')
+    help_text='포스팅 제목을 입력하세요. 100자 내외')
     content = models.TextField(verbose_name='내용') #길이제한 없음
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, blank=True,
@@ -29,11 +30,16 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True) #최초 생성시간 저장
     updated_at = models.DateTimeField(auto_now = True)  #갱신 때마다 시간 저장
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
+
     class Meta:
         ordering = ['-id']
 
     def __str__(self):
         return self.title
+
+
 
 
 class Comment(models.Model):
--- blog/url.py
+++ blog/url.py
@@ -1,7 +1,10 @@
-from django.urls import re_path
+from django.urls import re_path, path
+from blog import views_cbv
 from .import views
 
 urlpatterns = [
     re_path(r'^$', views.post_list, name='post_list'),
     re_path(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
+
+    path('cbv/new/', views_cbv.post_new),
 ]
