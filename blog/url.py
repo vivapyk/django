da55ebe6a38ed69bb7673ffdd6ba432e185bcd63
vivapyk <git hub vivapyk@gmail.com>
2019-01-28 02:28:26 +0900

    url name 지어주기

--- blog/url.py
+++ blog/url.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from .import views
 
 urlpatterns = [
-    re_path(r'^$', views.post_list),
-    re_path(r'^(?P<id>\d+)/$', views.post_detail),
+    re_path(r'^$', views.post_list, name='post_list'),
+    re_path(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
 ]
