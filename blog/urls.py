d753a3407dd65a56080d730b7e28abb7a7898145
vivapyk <git hub vivapyk@gmail.com>
2019-01-30 04:47:05 +0900

    get_absolute_url 해결

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import re_path, path
 from blog import views_cbv
+from blog.views import post_edit
 from .import views
 
 urlpatterns = [
     re_path(r'^$', views.post_list, name='post_list'),
-    re_path(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
-
+    path('<int:id>/', views.post_detail, name='post_detail'),
     path('new/', views.post_new, name='post_new'),
-    path('<int:id>/edit/', views.post_edit, name='post_edit'),
+    path('<int:id>/edit/', post_edit, name='post_edit'),
     path('cbv/new/', views_cbv.post_new),
 ]
