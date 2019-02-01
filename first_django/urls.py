71736d9d478ce7107fb4faea2fab2dd49e82a03b
vivapyk <git hub vivapyk@gmail.com>
2019-02-01 15:32:06 +0900

    media 파일 서빙

--- first_django/urls.py
+++ first_django/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path
@@ -34,6 +35,8 @@ urlpatterns = [
     path('dojo/', include(('dojo.url', 'dojo'), namespace='dojo')),
     path('shop/', include(('shop.urls', 'shop'), namespace='shop')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
