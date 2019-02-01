9bcc79d8f0a852b3ac53e2f798e7400192bb86a6
vivapyk <git hub vivapyk@gmail.com>
2019-02-01 16:36:29 +0900

    원본을 유지하면서 썸네일관리

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 import re
 from django.forms import ValidationError
 from django.urls import reverse
+from imagekit.models import ImageSpecField
+from pilkit.processors import Thumbnail
 
 
 def lnglat_validator(value):
@@ -23,6 +25,7 @@ class Post(models.Model):
         help_text='포스팅 제목을 입력하세요. 100자 내외.')
     content = models.TextField(verbose_name='내용') #길이제한 없음
     photo = models.ImageField(blank=True, upload_to='blog/post/%Y/%m/%d')
+    photo_thumbnail = ImageSpecField(source='photo', processors=[Thumbnail(300, 300)], format='JPEG', options={'quality':60})
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, blank=True,
                 validators=[lnglat_validator], help_text='경도/위도')
