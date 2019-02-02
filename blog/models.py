f67df85a86800573d652ffc6054505c8b2c8e299
vivapyk <git hub vivapyk@gmail.com>
2019-02-02 17:53:14 +0900

    imagekit활용해서 썸네일만들기

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 import re
 from django.forms import ValidationError
 from django.urls import reverse
+from imagekit.models import ProcessedImageField
 from imagekit.models import ImageSpecField
 from pilkit.processors import Thumbnail
 
@@ -24,8 +25,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name='제목',# 길이제한 100
         help_text='포스팅 제목을 입력하세요. 100자 내외.')
     content = models.TextField(verbose_name='내용') #길이제한 없음
-    photo = models.ImageField(blank=True, upload_to='blog/post/%Y/%m/%d')
-    photo_thumbnail = ImageSpecField(source='photo', processors=[Thumbnail(300, 300)], format='JPEG', options={'quality':60})
+    photo = ProcessedImageField(blank=True, upload_to='blog/post/%Y/%m/%d', processors=[Thumbnail(300, 300)], format="JPEG", options={'quality': 60})
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, blank=True,
                 validators=[lnglat_validator], help_text='경도/위도')
