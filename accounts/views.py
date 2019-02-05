e1b52fe1629dcebef04bc94c67704ec06860c26d
vivapyk <git hub vivapyk@gmail.com>
2019-02-06 03:23:00 +0900

    회원가입에 email 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
 
 # Create your views here.
+from accounts.forms import SignupForm
+
+
 @login_required
 def profile(request):
     return render(request, 'accounts/profile.html')
@@ -12,12 +14,12 @@ def profile(request):
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             user=form.save()
             return redirect(settings.LOGIN_URL)
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'accounts/signup_form.html', {
         'form': form,
     })
