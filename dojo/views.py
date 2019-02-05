6f4dce9e436e840ef64ecb0a3a95b785e2470233
vivapyk <git hub vivapyk@gmail.com>
2019-02-05 23:13:32 +0900

    CBV sample Step 4

--- dojo/views.py
+++ dojo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse, HttpResponse
 import os
 # Create your views here.
+from django.views.generic import DetailView
 
 from dojo.forms import PostForm
 from dojo.models import Post
@@ -91,27 +92,5 @@ def post_list3(request):
 #         return response
 
 
-class DetailView(object):
-    def __init__(self, model):
-        self.model = model
-
-    def get_object(self, *args, **kwargs):
-        return get_object_or_404(self.model, id=kwargs['id'])
-
-    def get_template_name(self):
-        return '{}/{}_detail.html'.format(self.model._meta.app_label, self.model._meta.model_name)
-
-    def dispatch(self, request, *args, **kwargs):
-        return render(request, self.get_template_name(), {
-            self.model._meta.model_name: self.get_object(*args, **kwargs),
-        })
-
-    @classmethod
-    def as_view(cls, model):
-        def view(request, *args, **kwargs):
-            self = cls(model)
-            return self.dispatch(request, *args, **kwargs)
-        return view
-
-post_detail = DetailView.as_view(Post)
+post_detail = DetailView.as_view(model=Post, pk_url_kwarg='id')
 
