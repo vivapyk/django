5dcd4462606f2efafbd4660eb9c62275ac296c7c
vivapyk <git hub vivapyk@gmail.com>
2019-02-06 00:33:01 +0900

    회원가입 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('profiles/', views.profile)
+    path('profiles/', views.profile),
+    path('signup/', views.signup, name='signup')
 ]
