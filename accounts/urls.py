bbab04fc2d13c331f6ba2a3371f17018e21546fe
vivapyk <git hub vivapyk@gmail.com>
2019-02-06 04:07:45 +0900

    퀴즈풀이?

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,16 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.urls import path
+
+from accounts.forms import LoginForm
 from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login_form.html'), name='login',
+         kwargs={'authentication_form': LoginForm, }),
     path('logout/', auth_views.LogoutView.as_view(next_page=settings.LOGIN_URL), name='logout'),
 ]
 
