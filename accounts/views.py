35bbb878910b384b5770df79db082aef8b602126
vivapyk <git hub vivapyk@gmail.com>
2019-02-06 03:11:55 +0900

    login 일때만 profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
 
 # Create your views here.
+@login_required
 def profile(request):
     return render(request, 'accounts/profile.html')
 
