637e91087174f9126631c569d6258b060e954138
vivapyk <git hub vivapyk@gmail.com>
2019-02-05 23:45:53 +0900

    DeleteView 구현

--- blog/urls.py
+++ blog/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('<int:id>/edit/', post_edit, name='post_edit'),
     path('cbv/new/', views_cbv.post_new),
     path('cbv/<int:pk>/edit', views_cbv.post_edit),
+    path('cbv/<int:pk>/delete', views_cbv.post_delete),
+
 ]
--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.views.generic import CreateView, ListView, DetailView, UpdateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -11,3 +11,4 @@ post_detail = DetailView.as_view(model=Post)
 
 post_edit = UpdateView.as_view(model=Post, fields='__all__')
 
+post_delete = DeleteView.as_view(model=Post, success_url='/blog/')
